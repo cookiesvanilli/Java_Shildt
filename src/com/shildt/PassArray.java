62f9e1f114734ba6016f631c16678554b4f37610
inna <in.d2010@yande.ru>
2022-04-25 20:14:22 +0300

    variable length args

---
+++ src/com/shildt/PassArray.java
@@ -0,0 +1,23 @@
+package com.shildt;
+
+public class PassArray {
+    static void vaTest(int v[]) {
+        System.out.println("Количество аргументов: " + v.length
+                + " Содержимое: ");
+
+        for (int x : v) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int n1[] = {10};
+        int n2[] = {1, 2, 3};
+        int n3[] = {};
+
+        vaTest(n1);
+        vaTest(n2);
+        vaTest(n3);
+    }
+}
---
+++ src/com/shildt/VarArgs.java
@@ -0,0 +1,19 @@
+package com.shildt;
+
+public class VarArgs {
+    static void vaTest(int... v) {
+        System.out.println("Количество аргументов: " + v.length
+                + " Содержимое: ");
+
+        for (int x : v) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        vaTest(10);
+        vaTest(1, 2, 3);
+        vaTest();
+    }
+}
